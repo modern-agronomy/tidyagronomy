0469187f39f039b26c8816de3fa5d85c86606f1a
James Horine <jamespatrickhorine@gmail.com>
2023-09-21 15:30:33 -0500

    updates

--- R/read_data.R
+++ R/read_data.R
@@ -5,7 +5,7 @@
 read_era5 <- NULL
 read_era5 <- function(filePath){
   tidync::tidync(x = filePath) %>%
-    tidync::hyper_tibble %>%
+    hyper_tibble %>%
     dplyr::mutate(time = lubridate::as_datetime(time*60*60, origin = '1900-01-01 00:00:00.0' ))
 }
 
