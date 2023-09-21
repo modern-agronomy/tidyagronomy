7c4329465f5e7468903bc401d19f843ca411636b
James Horine <jamespatrickhorine@gmail.com>
2023-09-21 08:15:42 -0500

    updating reader functions

--- R/read_data.R
+++ R/read_data.R
@@ -6,7 +6,7 @@ read_era5 <- NULL
 read_era5 <- function(filePath){
   tidync::tidync(x = filePath) %>%
     tidync::hyper_tibble %>%
-    dplyr::mutate(time = as_datetime(time*60*60, origin = '1900-01-01 00:00:00.0' ))
+    dplyr::mutate(time = lubridate::as_datetime(time*60*60, origin = '1900-01-01 00:00:00.0' ))
 }
 
 
